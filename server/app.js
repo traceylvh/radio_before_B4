cd7a61c30c0de9f103d70863cfb0696fca582247
tracey van haaften <tracey.lee.213@gmail.com>
2016-04-15 21:01:44 -0500

    added font awesome, styling

--- server/app.js
+++ server/app.js
@@ -42,9 +42,9 @@ app.use(passport.session());
 //  process.env.MONGOLAB_URI ||
 //  process.env.MONGOHQ_URL ||
 
-// var mongoURI = 'mongodb://localhost/radio_before';
+var mongoURI = 'mongodb://localhost/radio_before';
 
-var mongoURI = "ds025180.mlab.com:25180/radio_before";
+// var mongoURI = "ds025180.mlab.com:25180/radio_before";
 var MongoDB = mongoose.connect(mongoURI).connection;
 
 MongoDB.on("error", function(err){
